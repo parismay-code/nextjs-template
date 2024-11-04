21fceed9a190438799b6d197895cc71c28f1bae2
Kirill Kuznetsov <kka.fullstack@gmail.com>
2024-11-04 11:12:20 +0300

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import { inter } from "@/app/ui/fonts";
+import { PropsWithChildren } from "react";
+import '@/app/ui/global.css';
+
+export default function RootLayout({ children }: PropsWithChildren) {
+		return (
+				<html lang="en">
+				<body className={ `${ inter.className } antialiased` }>{ children }</body>
+				</html>
+		);
 }
